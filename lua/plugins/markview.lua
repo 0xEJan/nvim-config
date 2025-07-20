bcfa1f495a2748ed7b4cd83881069af83150d694
0xEJan <81864871+0xEJan@users.noreply.github.com>
2025-07-20 22:57:05 +0200

    Adding config for Markview plugin

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,15 @@
+local markview = {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+
+   -- For `nvim-treesitter` users.
+    priority = 49,
+
+    -- For blink.cmp's completion
+    -- source
+    -- dependencies = {
+    --     "saghen/blink.cmp"
+    -- },
+};
+
+return markview
