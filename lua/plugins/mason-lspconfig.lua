09b27ecd9a690bfb27c35d7b6e1ae50c991df2a9
0xEJan <81864871+0xEJan@users.noreply.github.com>
2025-12-04 00:40:04 +0100

    initial working mason config

---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,13 @@
+return {
+  "mason-org/mason-lspconfig.nvim",
+  enabled = true,
+  lazy = false,
+  dependencies = {
+    "mason-org/mason.nvim",
+    "neovim/nvim-lspconfig"
+  },
+  opts = {
+    ensure_installed = {
+    },
+  }
+}
---
+++ lua/plugins/mason-tool-installer.lua
@@ -0,0 +1,12 @@
+return {
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
+  enabled = true,
+  lazy = false,
+  dependencies = {
+    "mason-org/mason.nvim",
+  },
+  opts = {
+    ensure_installed = {
+    },
+  }
+}
